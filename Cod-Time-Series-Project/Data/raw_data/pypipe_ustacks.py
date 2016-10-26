973910ecc5829b3dc29a76186f33e1072ca91d77
nclowell <nclowell@uw.edu>
2016-10-26 15:33:22 -0700

    making ustacks pipeline

--- Cod-Time-Series-Project/Data/raw_data/pypipe_ustacks.py
+++ Cod-Time-Series-Project/Data/raw_data/pypipe_ustacks.py
@@ -14,6 +14,7 @@ import sys
 # **Output** 4 files - alleles, modules, snps, tags
 
 
+
 # ---A) Rename your files by the sample name
 
 # you must first make a tab delimited text file where first col = barcode & second col = unique sample name
@@ -22,9 +23,9 @@ myfile = open(sys.argv[1], "r")				# myfile =  CSV mentioned, called after your 
 new_file = open("new_filenames1.txt", "w") # new txt file
 for line in myfile:						   # loop through each line
 	linelist=line.strip().split()		   # splits into list by white space
-	barcodefile = linelist[0] + "_1.fa.gz"
+	barcodefile = linelist[0] + "_1.fq.gz"
 	# print barcodefile 				   # troubleshooting loop
-	samplefile = linelist[1] + "_1.fa.gz"
+	samplefile = linelist[1] + "_1.fq.gz"
 	# print samplefile 					   # troubleshooting loop
 	newstring = "mv" + "\t" + barcodefile + "\t" + samplefile + "\n"
 	# print newstring  # troubleshooting loop
@@ -36,7 +37,31 @@ new_file.close()
 subprocess.call(['cd /users/natalielowell/Git-repos/FISH546/Cod-Time-Series-Project/Data/raw_data'], shell=True)
 
 # run the script you just made as a shell script to rename your files
-subprocess.call(['pwd'], shell=True)
+subprocess.call(['sh new_filenames1.txt'], shell=True)
+
+
+
+# ---B) Make shell script to run all samples through command line through ``ustacks``
+
+# ``ustacks`` requires an arbitrary integer for every sample, although unclear how it gets used as it does not become the name of the file
+
+ID_int = 001
+for line in new_filenames1.txt: 			#for each line in the barcode file	
+	linelist=line.strip().split()	
+	sampID = linelist[2] 		#save the third object as "sampID"
+	if ID_int < 10: 
+		ustacks_code = "ustacks -t gzfasta -f samplesT146/" + sampID + " -r -d -o stacks -i 00" + str(ID_int) + " -m 5 -M 3 -p 10" + "\n"
+								#create a line of code for ustacks that includes the new sample ID (with 2 leading 0s)
+	elif ID_int >= 10 & ID_int < 100: 
+		ustacks_code = "ustacks -t gzfasta -f samplesT146/" + sampID + " -r -d -o stacks -i 0" + str(ID_int) + " -m 5 -M 3 -p 10" + "\n"
+								#create a line of code for ustacks that includes the new sample ID (with 1 leading 0)
+	else: 
+		ustacks_code = "ustacks -t gzfasta -f samplesT146/" + sampID + " -r -d -o stacks -i " + str(ID_int) + " -m 5 -M 3 -p 10" + "\n"
+								#create a line of code for ustacks that includes the new sample ID (with no leading 0s)
+	newfile.write(ustacks_code)	#append this new line of code to the output file
+	ID_int += 1
+myfile.close()
+newfile.close()
 
 
 
@@ -46,14 +71,6 @@ subprocess.call(['pwd'], shell=True)
 
 
 
-# ---B) Produce a shell script that will run ``ustacks`` on all of your files, given desired parameters
 
 
 
-
-
-
-
-
-# Simple command
-# subprocess.call(['ls'], shell=True)
