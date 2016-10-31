14f2668641e704f34960e49d5ffb744fd25c6bb0
nclowell <nclowell@uw.edu>
2016-10-31 15:10:21 -0700

    updated cstacks script to include word line count and sort

--- Cod-Time-Series-Project/Scripts/pypipe_cstacks.py
+++ Cod-Time-Series-Project/Scripts/pypipe_cstacks.py
@@ -14,6 +14,8 @@ import sys
 # **Input** ustacks out put files of ~10 individuals ?
 # **Output** ?
 
+
+
 # ---A) COUNT LINES IN EACH FASTQ FILE. You want to use ~10 individuals to make your 
 #		catalog, and you want those individuals to have the most sequence reads. You also
 #		want representation from each of your populations. So fist, a script to get line 
@@ -23,9 +25,12 @@ myfile = open(sys.argv[1], "r")	#open the file with your list of barcodes and sa
 newfile = open("cstacks_linecount_shell.txt", "w")	#create a new file where the ustacks code will go
 line_cd = sys.argv[2]
 filestring = "cd " + line_cd + "\n"
+samplename_list = [] # to be used in loop later in this script
+
 for line in myfile: 					#for each line in the barcode file
 	linelist = line.strip().split()		#make a list of character strings broken by tabs
 	sampID = linelist[2]				#pick out file name
+	samplename_list.append(sampID)
 	newstring = "gunzip -c " + sampID + " | wc -l >> cstacks_linecount.txt\n" # make line of code to run at command line
 	filestring += newstring # add to a list of strings we'll write to a file
 myfile.close()
@@ -33,11 +38,42 @@ print filestring
 newfile.write(filestring) # write to file
 newfile.close()
 
-
-# ---B) Create a tuple that has the filename and the number of lines, and then sort in  
-#		descending order to pick the most prolific ten individuals. 
-
-for line in 
-
 # run shell script that will calculate line counts
 subprocess.call(['sh cstacks_linecount_shell.txt'], shell=True)
+
+# *** if you have to rerun this script, it will append onto it! make sure no file w name!
+
+
+
+# ---B) Create a list of lists that has the filename and the number of lines in each 
+#		sublist, and then sort in descending order of second sublist item to pick the most 
+#		prolific ten individuals. 
+
+linecounts = open("cstacks_linecount.txt", "r") # read in line counts file
+linecounts_list = [] # initiate a list for the line counts so I can get.item later
+
+for line in linecounts:
+	count = line.strip().split() # get line count
+	linecounts_list.append(line)
+
+list_samp_ct = [] # initiate empty list
+i = 0 # start counter
+
+# --- CHECK^ if lists look normal
+# print "sample name list "
+# print samplename_list
+# print "line counts list "
+# print linecounts_list
+
+for item in linecounts_list:
+	new_item = [samplename_list[i], linecounts_list[i]]
+	list_samp_ct.append(new_item)
+	i += 1
+	
+def getKey(item): # so that sorted will sort by second item in list
+	return item[1]
+sortedlist = sorted(list_samp_ct, key = getKey, reverse = True)
+print sortedlist
+
+with open('all_sorted_name_counts.txt', 'w') as file:
+    file.writelines('\t'.join(i) + '\n' for i in sortedlist)
