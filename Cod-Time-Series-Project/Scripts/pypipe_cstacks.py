9c38cacfaadea38f6ac4b3babe087f7a541a7c43
nclowell <nclowell@uw.edu>
2016-10-31 16:40:29 -0700

    ustacks code that works! updated

--- Cod-Time-Series-Project/Scripts/pypipe_cstacks.py
+++ Cod-Time-Series-Project/Scripts/pypipe_cstacks.py
@@ -2,14 +2,14 @@
 # Pacific Cod Time Series Project #
 
 #### WHEN RUNNING THIS SCRIPT
-## INPUTS: python [pypipe_cstacks.py] [txt file with sample names] [wd for data files] [# individuals for cstacks] [batch number]
+## --- INPUTS at command line: python {0}[pypipe_cstacks.py] {1}[txt file with sample names] 
+##		{2}[wd for data files] {3}[# individuals for cstacks] {4}[batch number] {5}[output directory] 
+##		{6}[num mismatches allowed] {7}[num threads]
+## --- DEPENDENCIES: your file names coming out of ustacks cannot have a period other than before file extension
+# 		call necessary modules
 
-# call necessary modules
-
-import subprocess # call module that will run shell scripts from this python script
 import sys 
-import os
-from os.path import basename
+import subprocess # call module that will run shell scripts from this python script
 
 ### ``cstacks``
 # **PURPOSE*** ustacks creates a catalog from a subset of individuals to call SNPs
@@ -26,7 +26,7 @@ from os.path import basename
 myfile = open(sys.argv[1], "r")	#open the file with your list of barcodes and sample IDs
 newfile = open("cstacks_linecount_shell.txt", "w")	#create a new file where the ustacks code will go
 line_cd = sys.argv[2]
-filestring = "cd " + line_cd + "\n"
+filestring = ""
 samplename_list = [] # to be used in loop later in this script
 
 for line in myfile: 					#for each line in the barcode file
@@ -36,7 +36,7 @@ for line in myfile: 					#for each line in the barcode file
 	newstring = "gunzip -c " + sampID + " | wc -l >> cstacks_linecount.txt\n" # make line of code to run at command line
 	filestring += newstring # add to a list of strings we'll write to a file
 myfile.close()
-print filestring
+# print filestring # CHECK^
 newfile.write(filestring) # write to file
 newfile.close()
 
@@ -75,23 +75,31 @@ for item in linecounts_list:
 def getKey(item): # so that sorted will sort by second item in list
 	return item[1]
 sortedlist = sorted(list_samp_ct, key = getKey, reverse = True)
-print sortedlist
+# print sortedlist # CHECK^
 
 with open('all_sorted_name_counts.txt', 'w') as file:
-    file.writelines('\t'.join(i) + '\n' for i in sortedlist)
+    file.writelines('\t'.join(i) + '\n' for i in sortedlist) # makes file
     
-    
-# cstacks_shell = open("cstacks_shell.txt", "w")
-# firstline = "cstacks -b " + sys.argv[4] + "\n"
-# 
-# 
-# endrange = sys.argv[3]+1 # set end of range for loop
-# 
-# for i in range(0, endrange):
-# 	filenames = 
 
-# cstacks_shell.write(firstline)
 
-print(basename("/a/b/c.txt"))
+
+# --- C) Write shell script that will run ``cstacks``
+
+cstacks_shell = ""
+firststr = "cstacks -b " + sys.argv[4] + " "
+cstacks_shell += firststr
+
+endrange = int(sys.argv[3]) # set end of range for loop
+for i in range(0, endrange):
+	filename = samplename_list[i]
+	trmd_filename = filename.rsplit(".",2)[0]
+	# print trmd_filename # CHECK^
+	string = "-s " + trmd_filename + " "
+	cstacks_shell += string
+laststr = "-o " + sys.argv[5] + " -n " + sys.argv[6] + " -p " + sys.argv[7]
+cstacks_shell += laststr
+# print cstacks_shell # CHECK^
+
+
 
  
