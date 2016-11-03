9c6e76432fabbbe8f877a802078dbc5bd7c4dbec
nclowell <nclowell@uw.edu>
2016-11-03 14:09:24 -0700

    updated cstacks script

--- Cod-Time-Series-Project/Scripts/pypipe_cstacks.py
+++ Cod-Time-Series-Project/Scripts/pypipe_cstacks.py
@@ -1,40 +1,46 @@
-###--- Natalie's Python Pipeline for Processing RAD data ---###
-# Pacific Cod Time Series Project #
+##########################################################################################
 
-#### WHEN RUNNING THIS SCRIPT
-## INPUTS at command line: 
+###--- ``cstacks`` script
+#
+# PURPOSE: ustacks creates a catalog from a subset of individuals to call SNPs
+# INPUT: ustacks out put files for specified number of individuals with most sequence reads
+# OUTPUT: catalog file + associated files
+
+#### WHEN RUNNING THIS SCRIPT, INPUTS AT THE COMMAND LINE ARE:
 # python 
 # {0}[pypipe_cstacks.py] 
-# {1}[txt file with sample names] 	
-# {2}[wd for data files] 
+# {1}[shell script with changed file names] 	
+# {2}[directory of input files] 
 # {3}[# individuals for cstacks] 
 # {4}[batch number] 
 # {5}[output directory] 
 # {6}[num mismatches allowed] 
 # {7}[num threads]
 
-## --- DEPENDENCIES: your file names coming out of ustacks cannot have a period other than before file extension
+### DEPENDENCIES: 
+# [1] Your file names coming out of ustacks cannot have a period other than before file extension (not sure if true! check?)
 
-### ---  Call necessary modules
+### WARNINGS:
+# [1] If you have to rerun this script, it will append onto it! make sure no file w name or else won't even run!
+
+##########################################################################################
+
+### --- [A] Call necessary modules
 
 import sys 
-import subprocess # call module that will run shell scripts from this python script
-
-### ``cstacks``
-# **PURPOSE*** ustacks creates a catalog from a subset of individuals to call SNPs
-# **Input** ustacks out put files of ~10 individuals ?
-# **Output** ?
+import subprocess
 
 
 
-# ---A) COUNT LINES IN EACH FASTQ FILE. You want to use ~10 individuals to make your 
-#		catalog, and you want those individuals to have the most sequence reads. You also
-#		want representation from each of your populations. So fist, a script to get line 
-#		count for each file.
+
+
+### --- [B] Count lines in each sequence file
 
 myfile = open(sys.argv[1], "r")	#open the file with your list of barcodes and sample IDs
-newfile = open("cstacks_linecount_shell.txt", "w")	#create a new file where the ustacks code will go
-line_cd = sys.argv[2]
+
+dirfrom = sys.argv[2] # get directory for input files
+firststr = "cd " + "sys.argv[2]" + "\n" # write first line of shell to cd to this directory
+
 filestring = ""
 samplename_list = [] # to be used in loop later in this script
 
@@ -46,19 +52,23 @@ for line in myfile: 					#for each line in the barcode file
 	filestring += newstring # add to a list of strings we'll write to a file
 myfile.close()
 # print filestring # CHECK^
-newfile.write(filestring) # write to file
+
+
+filestring = firststr + filestring # write cd line before rest of file string
+
+#create a new file where the ustacks code will go, write string to file, close
+newfile = open("cstacks_linecount_shell.txt", "w")
+newfile.write(filestring)
 newfile.close()
 
 # run shell script that will calculate line counts
 subprocess.call(['sh cstacks_linecount_shell.txt'], shell=True)
 
-# *** if you have to rerun this script, it will append onto it! make sure no file w name!
 
 
 
-# ---B) Create a list of lists that has the filename and the number of lines in each 
-#		sublist, and then sort in descending order of second sublist item to pick the most 
-#		prolific ten individuals. 
+
+### --- [C] Get sample names for specified # of samples with most sequence reads 
 
 linecounts = open("cstacks_linecount.txt", "r") # read in line counts file
 linecounts_list = [] # initiate a list for the line counts so I can get.item later
@@ -92,7 +102,8 @@ with open('all_sorted_name_counts.txt', 'w') as file:
 
 
 
-# --- C) Write shell script that will run ``cstacks``
+
+### --- [D] Write and run shell script for ``cstacks``
 
 cstacks_shell = ""
 firststr = "cstacks -b " + sys.argv[4] + " "
@@ -108,6 +119,15 @@ for i in range(0, endrange):
 laststr = "-o " + sys.argv[5] + " -n " + sys.argv[6] + " -p " + sys.argv[7]
 cstacks_shell += laststr
 # print cstacks_shell # CHECK^
+
+cstacks_shell_txt = write("cstacks_shell.txt", "w")
+cstacks_shell_txt.write(cstacks_shell)
+cstacks_shell_txt.close()
+
+# run shell script
+subprocess.call(["cstacks_shell.txt"], shell = True)
+
+##########################################################################################
 
 ### --- DOCUMENTATION FOR CSTACKS
 
@@ -126,7 +146,3 @@ cstacks_shell += laststr
 # Advanced options:
 # 
 # --report_mmatches — report query loci that match more than one catalog locus.
-
-
-
- 
